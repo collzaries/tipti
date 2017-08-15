b70926f9b3d98fefef7d414412568541af8a175b
Renny Jose Luzardo Colina <collzaries@collz.aries>
2017-08-15 02:23:26 -0400

    tipti web maqueta header y sub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubHeaderComponent
   ],
   imports: [
     BrowserModule
